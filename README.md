67df9b7a88ec3db825edd7277ac6e2002f2b6b71
Thiago Arrais <thiago.souza@serpro.gov.br>
2019-02-25 14:22:23 -0300

    versao inicial

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # httpmodule-codemod
 
+Codemod para portar projetos Angular de `Http` para `HttpClient`.
+
+Status: experimental
+
+## Como usar
+
+```bash
+$ npm install -g jscodeshift
+$ jscodeshift -t https://raw.githubusercontent.com/thiagoarrais/httpmodule-codemod/master/transforms/httpmodule.js
+  --extensions=ts \
+  --parser=ts \
+  meu-projeto-angular/
+```
---
+++ transforms/httpmodule.js
@@ -0,0 +1,83 @@
+const transform = (fileInfo, api, options) => {
+  const j = api.jscodeshift;
+
+  const convertImport = (module) => (p) => {
+    const modifiedNode = p.node;
+    modifiedNode.specifiers[0] = j.importSpecifier(
+      j.identifier(module)
+    );
+
+    modifiedNode.source = j.stringLiteral('@angular/common/http')
+    return modifiedNode;
+  };
+
+  const removeMapToJson = (p) => {
+    return p.node.callee.object;
+  };
+
+  const convertImportHttpModule = convertImport('HttpClientModule');
+  const convertImportHttp = convertImport('HttpClient');
+
+  const originalAst = j(fileInfo.source);
+
+  const afterReplacingModuleImports = originalAst
+    .find(j.ImportDeclaration, {
+      specifiers: [{
+        type: "ImportSpecifier",
+        imported: {
+          name: "HttpModule"
+        }
+      }],
+      source: {
+        value: "@angular/http"
+      }
+    })
+    .replaceWith(convertImportHttpModule)
+    .toSource();
+
+  const afterReplacingHttpImports = j(afterReplacingModuleImports)
+    .find(j.ImportDeclaration, {
+      specifiers: [{
+        type: "ImportSpecifier",
+        imported: {
+          name: "Http"
+        }
+      }],
+      source: {
+        value: "@angular/http"
+      }
+    })
+    .replaceWith(convertImportHttp)
+    .toSource();
+
+
+  const afterReplacingJsonMappings = j(afterReplacingHttpImports)
+    .find(j.CallExpression, {
+      callee: {
+        type: "MemberExpression",
+        object: {
+          type: "CallExpression",
+          callee: {
+            type: "MemberExpression",
+            object: {
+              type: "MemberExpression",
+              property: {
+                type: "Identifier",
+                name: "http"
+              }
+            }
+          }
+        },
+        property: {
+          type: "Identifier",
+          name: "map"
+        }
+      }
+    })
+    .replaceWith(removeMapToJson)
+    .toSource();
+
+  return afterReplacingJsonMappings;
+};
+
+export default transform;
